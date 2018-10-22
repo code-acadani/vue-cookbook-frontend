6af23b474cc881ccca9a435039de37d7a18eface
danizaghian <dani.zaghian@gmail.com>
2018-10-22 10:48:08 -0700

    adds recipes new form

--- src/router.js
+++ src/router.js
@@ -3,25 +3,21 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Test from './views/Test.vue'
+import Signup from "./views/Signup.vue";
+import Login from "./views/Login.vue";
+import Logout from "./views/Logout.vue";
+import RecipesNew from "./views/RecipesNew.vue";
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: Test
-    }
+    { path: '/', name: 'home', component: Home },
+    { path: '/about', name: 'about', component: About },
+    { path: '/test', name: 'test', component: Test },
+    { path: "/signup", name: "signup", component: Signup },
+    { path: "/login", name: "login", component: Login },
+    { path: "/logout", name: "logout", component: Logout },
+    { path: "/recipes/new", name: "recipes-new", component: RecipesNew }
   ]
 })
---
+++ src/views/RecipesNew.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="recipes-new">
+    <div class="container">
+      <form v-on:submit.prevent="submit()">
+        <h1>New Recipe</h1>
+        <ul>
+          <li class="text-danger" v-for="error in errors">{{ error }}</li>
+        </ul>
+        <div class="form-group">
+          <label>Title:</label> 
+          <input type="text" class="form-control" v-model="title">
+        </div>
+        <div class="form-group">
+          <label>Ingredients:</label>
+          <input type="text" class="form-control" v-model="ingredients">
+        </div>
+        <div class="form-group">
+          <label>Directions:</label>
+          <input type="text" class="form-control" v-model="directions">
+        </div>
+        <div class="form-group">
+          <label>Image Url:</label>
+          <input type="text" class="form-control" v-model="imageUrl">
+        </div>
+        <div class="form-group">
+          <label>Prep Time:</label>
+          <input type="number" class="form-control" v-model="prepTime">
+        </div>
+        <input type="submit" class="btn btn-primary" value="Submit">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      title: "",
+      ingredients: "",
+      directions: "",
+      imageUrl: "",
+      prepTime: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        title: this.title,
+        ingredients: this.ingredients,
+        directions: this.directions,
+        image_url: this.imageUrl,
+        prep_time: this.prepTime
+      };
+      axios
+        .post("http://localhost:3000/api/recipes", params)
+        .then(response => {
+          this.$router.push("/");
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    }
+  }
+};
+</script>
